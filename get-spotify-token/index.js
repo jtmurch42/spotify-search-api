7ff22f8eae8eaa873f406b40c13f0d425567283a
Jeremy Murchison <jtmurch92@gmail.com>
2022-01-27 22:23:37 -0700

    Change to use async/await and try/catch

--- get-spotify-token/index.js
+++ get-spotify-token/index.js
@@ -1,29 +1,27 @@
 const axios = require('axios');
 
-exports.handler = () => {
+exports.handler = async () => {
   const response = {
     headers: {
       'Access-Control-Allow-Origin': '*'
     }
   };
 
-  return axios
-    .post('https://accounts.spotify.com/api/token', 'grant_type=client_credentials', {
+  try {
+    const result = await axios.post('https://accounts.spotify.com/api/token', 'grant_type=client_credentials', {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
         Authorization: `Basic ${Buffer.from(
           process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
         ).toString('base64')}`
       }
-    })
-    .then((res) => {
-      response.statusCode = 200;
-      response.body = JSON.stringify(res.data);
-      return response;
-    })
-    .catch((error) => {
-      response.statusCode = error.response.status;
-      response.body = JSON.stringify(error.response.data);
-      return response;
     });
+    response.statusCode = 200;
+    response.body = JSON.stringify(result.data);
+    return response;
+  } catch (error) {
+    response.statusCode = error.response.status;
+    response.body = JSON.stringify(error.response.data);
+    return response;
+  }
 };
